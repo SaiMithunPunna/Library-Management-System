6ea7c6d1c3f8aafcbf587fd8d62123ca59564f40
SaiMithunPunna <saimithunamangal@gmail.com>
2026-04-19 21:06:01 +0530

    added Library class

---
+++ Library.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+import java.util.*;
+
+public class Library {
+    
+    private List<Book> books=new ArrayList<> ();
+    private List<User> users= new ArrayList<>();
+    private List<Transaction> transactions = new ArrayList<>();
+
+
+    
+
+
+}
