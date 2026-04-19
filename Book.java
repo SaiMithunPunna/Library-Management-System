f5b7c53895b5507c72b7f41b1e91c2d07c72f327
SaiMithunPunna <saimithunamangal@gmail.com>
2026-04-19 20:48:09 +0530

    Book class added

---
+++ Book.java
@@ -0,0 +1,35 @@
+public class Book
+{
+    private String title;
+    private int id ;
+    private String author;
+    private boolean isIssued;
+
+    public Book(int id ,String  title , String author )
+    {
+        this.title=title;
+        this.id=id;
+        this.author=author;
+        this.isIssued=false;
+    }
+    public int getId()
+    {
+        return this.id;
+    }
+    public String gettitle()
+    {
+        return this.title;
+    }
+    public String getAuthor()
+    {
+        return author;
+    }
+    public void setIssued(boolean issued)
+    {
+        this.isIssued=issued;
+    }
+    public String viewBook()
+    {
+        return id + " | " + title + " | " + author + " | Issued: " + isIssued;
+    }
+}
