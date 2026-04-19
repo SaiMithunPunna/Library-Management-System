52f02f818a33b90ec8b434a9d688aa7048e3ffe0
SaiMithunPunna <saimithunamangal@gmail.com>
2026-04-19 21:02:32 +0530

    added transaction class

---
+++ Transaction.java
@@ -0,0 +1,38 @@
+import java.time.LocalDate;
+public class Transaction {
+    private Book book;
+    private User user;
+    private LocalDate issueDate;
+    private LocalDate dueDate;
+
+    public Transaction(Book book , User user , LocalDate issueDate)
+    {
+        this.book=book;
+        this.user=user;
+        this.issueDate=issueDate;
+        this.dueDate=issueDate.plusDays(7);
+
+    }
+
+    public Book getBook()
+    {
+        return book;
+    }
+
+    public User getUser()
+    {
+        return user;
+    }
+
+    public LocalDate getIssueDate()
+    {
+        return issueDate;
+    }
+
+    public LocalDate getDueDate()
+    {
+        return dueDate;
+    }
+
+    
+}
